ee667153db16faf2a0cb130d5a5c695fb5427ff9
Ruan Aragão <ruanaragao2@gmail.com>
2023-05-08 00:38:10 -0300

    Add full width to button

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home() {
             <div className="py-3">
               <button
                 type="submit"
-                className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 Create game
               </button>
