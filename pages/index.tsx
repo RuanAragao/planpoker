7166a5a08467ef365745b79e1deffaf94cb32959
Ruan Aragão <ruanaragao2@gmail.com>
2023-05-08 00:13:55 -0300

    Recreate inital markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { v4 as uuidv4 } from 'uuid'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import api from '../utils/api';
 
 const inter = Inter({ subsets: ['latin'] })
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
-
 export default function Home() {
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyB6mO_X6srSdtJoGF5buU5OoaOWcQ3_rec",
-    authDomain: "planningpoker-51cf2.firebaseapp.com",
-    projectId: "planningpoker-51cf2",
-    storageBucket: "planningpoker-51cf2.appspot.com",
-    messagingSenderId: "865562708199",
-    appId: "1:865562708199:web:e432607d3ec543d2453a42",
-
-    databaseURL: "https://planningpoker-51cf2-default-rtdb.firebaseio.com/"
-  };
-
-  // Initialize Firebase
-  const firebase = initializeApp(firebaseConfig);
-
-  // Initialize Realtime Database and get a reference to the service
-  const database = getDatabase(firebase);
+  
+  const router = useRouter()
 
   const handlerSubmitted = async (event:any) => {
     event.preventDefault();
 
     const data = {
-      userId: uuidv4(),
+      gameId: uuidv4(),
       user: event.target.userName.value,
       room: event.target.roomName.value
     }
 
-    // const docRef = db.collection('users').doc('alovelace');
-
-    // await docRef.set({
-    //   first: 'Ada',
-    //   last: 'Lovelace',
-    //   born: 1815
-    // });
-
-    console.log(data.user, data.room);
-    writeUserData(data.userId, data.user, data.room);
-  };
-
-  async function writeUserData(userId:any, user:any, room:any) {
-    const db = getDatabase(firebase);
-    // const docRef = db.collection('users').doc('alovelace');
-
-    // await docRef.set({
-    //   first: 'Ada',
-    //   last: 'Lovelace',
-    //   born: 1815
-    // });
-    await set(ref(db, 'users/' + userId), {
-      userId,
-      user,
-      room
+    api.post('/games', data).then((response: { data: any }) => {
+      router.push(`/room/${response.data.id}`)
     });
   }
 
@@ -77,26 +34,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <header className="text-3xl font-bold">
-          Panning Poker
+      <main className="container mx-auto px-4 grid grid-flow-row h-screen justify-stretch">
+        <header className="flex justify-center items-center">
+          <h1 className="text-3xl font-bold text-center">PlanPoker</h1>
         </header>
-        <div className="md:grid md:grid-cols-3 md:gap-6 center">
+        <div className="grid md:grid-cols-3 gap-6 center">
           <form onSubmit={handlerSubmitted}>
             <div className="col-span-6 sm:col-span-3">
-              <label htmlFor="user-name" className="block text-sm font-medium text-gray-700">
-                User Name
-              </label>
-              <input
-                type="text"
-                id="user-name"
-                name="userName"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-3">
+              <h2>Insert the room’s name</h2>
               <label htmlFor="room-name" className="block text-sm font-medium text-gray-700">
-                Room Name
+                Game's Name
               </label>
               <input
                 type="text"
@@ -105,124 +52,17 @@ export default function Home() {
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               />
             </div>
-            <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+            <div className="py-3">
               <button
                 type="submit"
                 className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
-                Create
+                Create game
               </button>
             </div>
           </form>
         </div>
 
-
-
-
-
-
-        {/* <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
     </>
   )
